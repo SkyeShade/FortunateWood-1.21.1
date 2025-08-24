1a2b4ee3be8c3a06461739d6ec7143cb19320d33
SkyeShade <zpectspace@gmail.com>
2025-08-25 01:57:44 +0200

    Added a comment

--- src/main/java/com/skyeshade/fortunatewood/FortunateWood.java
+++ src/main/java/com/skyeshade/fortunatewood/FortunateWood.java
@@ -1,36 +1,17 @@
 package com.skyeshade.fortunatewood;
 
 import com.skyeshade.fortunatewood.loot.ModLootModifiers;
-import org.slf4j.Logger;
 
-import com.mojang.logging.LogUtils;
-
-import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.core.registries.Registries;
-import net.minecraft.network.chat.Component;
-import net.minecraft.world.food.FoodProperties;
-import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.CreativeModeTabs;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.material.MapColor;
-import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.Mod;
-import net.neoforged.fml.config.ModConfig;
+
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.neoforged.neoforge.common.NeoForge;
-import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
+
 import net.neoforged.neoforge.event.server.ServerStartingEvent;
-import net.neoforged.neoforge.registries.DeferredBlock;
-import net.neoforged.neoforge.registries.DeferredHolder;
-import net.neoforged.neoforge.registries.DeferredItem;
-import net.neoforged.neoforge.registries.DeferredRegister;
+
 
 
 @Mod(FortunateWood.MODID)
@@ -39,7 +20,7 @@ public class FortunateWood {
     public static final String MODID = "fortunatewood";
 
 
-
+    // main method
     public FortunateWood(IEventBus modEventBus, ModContainer modContainer) {
 
         modEventBus.addListener(this::commonSetup);
